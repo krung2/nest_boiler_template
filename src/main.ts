112c3df0591cc0ab2a414c450669756a3a40f187
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-14 15:13:07 +0900

    Add: validationPipe & swagger cli plugin

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
@@ -9,6 +9,7 @@ async function bootstrap() {
   const app: NestExpressApplication = await NestFactory.create<NestExpressApplication>(AppModule, { cors: false });
   const port: number = app.get(ConfigService).get('PORT');
   setUpSwagger(app);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, '0.0.0.0');
   Logger.log(`Application is running on: ${await app.getUrl()}`)
 }
