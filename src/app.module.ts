c7466b99298bf4edac66066b786f5a0d85a2a523
krung2 <jungbin4337@dgsw.hs.kr>
2021-10-14 14:33:58 +0900

    Add: configmodule env.example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
